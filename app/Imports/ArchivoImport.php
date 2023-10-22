e7b150352a694b64f255b379153e649469f17b75
jsponceA <jonny47169@gmail.com>
2023-10-22 16:02:13 -0500

    cambios

--- app/Imports/ArchivoImport.php
+++ app/Imports/ArchivoImport.php
@@ -31,7 +31,7 @@ class ArchivoImport implements WithHeadingRow, ToCollection
             Venta::query()->create([
                 "documento" => $row["documento"],
                 "vendedor" => $row["vendedor"],
-                "fecha" => strlen($fechaFinal) === 10 ? $fechaFinal : null,
+                "fecha" => strlen($fechaFinal) === 10 && now()->parse($fechaFinal)->isValid() ? $fechaFinal : null,
                 "nro_doc" => $row["nro_doc_cliente"],
                 "cliente" => $row["cliente"],
                 "cantidad" => is_numeric($row["cantidad"]) ? $row["cantidad"] : 0,
