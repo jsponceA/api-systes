8004a25a428509d2153fe3fb82611eaa2bfc4873
jsponceA <jonny47169@gmail.com>
2023-10-22 16:25:03 -0500

    cambios

--- app/Imports/ArchivoImport.php
+++ app/Imports/ArchivoImport.php
@@ -26,30 +26,32 @@ class ArchivoImport implements WithHeadingRow, ToCollection
                 $anio = $arrayFecha[2] ?? "";
 
                 $fechaFinal = $anio.'-'.$mes.'-'.$dia;
+
+                Venta::query()->create([
+                    "documento" => $row["documento"],
+                    "vendedor" => $row["vendedor"],
+                    "fecha" => strlen($fechaFinal) === 10 && checkdate($mes,$dia,$anio) ? $fechaFinal : null,
+                    "nro_doc" => $row["nro_doc_cliente"],
+                    "cliente" => $row["cliente"],
+                    "cantidad" => is_numeric($row["cantidad"]) ? $row["cantidad"] : 0,
+                    "u_medida" => $row["u_medida"],
+                    "codigo_producto" => $row["codigo_producto"],
+                    "producto" => $row["producto"],
+                    "moneda" => $row["moneda"],
+                    "precio_publico" => is_numeric($row["precio_publico"]) ? round($row["precio_publico"],2) : 0,
+                    "precio" => $row["precio"],
+                    "totales" => $row["totales"],
+                    "descuento" => $row["descuento_global"],
+                    "por_entregar" => $row["por_entregar"],
+                    "um_por_entregar" => $row["um_por_entregar"],
+                    "estado" => $row["estado"],
+                    "condicion_pago" => $row["condicion_de_pago"],
+                    "linea_padre" => $row["linea_padre"] ?? "",
+                    "linea_hijo" => $row["linea_hijo"] ?? "",
+                ]);
             }
 
-            Venta::query()->create([
-                "documento" => $row["documento"],
-                "vendedor" => $row["vendedor"],
-                "fecha" => strlen($fechaFinal) === 10 && checkdate($mes,$dia,$anio) ? $fechaFinal : null,
-                "nro_doc" => $row["nro_doc_cliente"],
-                "cliente" => $row["cliente"],
-                "cantidad" => is_numeric($row["cantidad"]) ? $row["cantidad"] : 0,
-                "u_medida" => $row["u_medida"],
-                "codigo_producto" => $row["codigo_producto"],
-                "producto" => $row["producto"],
-                "moneda" => $row["moneda"],
-                "precio_publico" => is_numeric($row["precio_publico"]) ? round($row["precio_publico"],2) : 0,
-                "precio" => $row["precio"],
-                "totales" => $row["totales"],
-                "descuento" => $row["descuento_global"],
-                "por_entregar" => $row["por_entregar"],
-                "um_por_entregar" => $row["um_por_entregar"],
-                "estado" => $row["estado"],
-                "condicion_pago" => $row["condicion_de_pago"],
-                "linea_padre" => $row["linea_padre"] ?? "",
-                "linea_hijo" => $row["linea_hijo"] ?? "",
-            ]);
+
         }
     }
 
